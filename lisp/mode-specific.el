ce26d291fd14712420b3740e432fe734104499bc
Andy Choens <andy.choens@gmail.com>
2015-04-02 21:27:11 -0400

    Moves snippets to analytic-snippets
    My snippet collection is likely to evolve quickly over the next few
    weeks and moving things back and forth between my Windows setup and my
    Linux setup would have gotten . . . . . tiresome. This should simplify
    my life.
    
    * lisp/mode-specific.el:
    
     Changes to be committed:
    	modified:	lisp/mode-specific.el
    	deleted:	snippets/org-mode/author
    	deleted:	snippets/org-mode/block
    	deleted:	snippets/org-mode/desc
    	deleted:	snippets/org-mode/dita
    	deleted:	snippets/org-mode/docbook
    	deleted:	snippets/org-mode/email
    	deleted:	snippets/org-mode/figure
    	deleted:	snippets/org-mode/html
    	deleted:	snippets/org-mode/inc
    	deleted:	snippets/org-mode/keywords
    	deleted:	snippets/org-mode/lang
    	deleted:	snippets/org-mode/latex
    	deleted:	snippets/org-mode/options
    	deleted:	snippets/org-mode/prop
    	deleted:	snippets/org-mode/res
    	deleted:	snippets/org-mode/sb
    	deleted:	snippets/org-mode/seq
    	deleted:	snippets/org-mode/src
    	deleted:	snippets/org-mode/startup
    	deleted:	snippets/org-mode/tags
    	deleted:	snippets/org-mode/text
    	deleted:	snippets/org-mode/title

--- lisp/mode-specific.el
+++ lisp/mode-specific.el
@@ -333,8 +333,9 @@
 (require 'yasnippet)
 
 (setq yas-snippet-dirs
-      '("~/.emacs.d/snippets"                 ;; personal snippets
-        ;;"/path/to/some/collection/"           ;; foo-mode and bar-mode snippet collection
+      '("~/.emacs.d/snippets"           ;; Personal snippets
+        "~/Git/analytic-snippets"       ;; Snippets distributed with my
+                                        ;; analytic templates.
         ;;"/path/to/yasnippet/yasmate/snippets" ;; the yasmate collection
         ;;"/path/to/yasnippet/snippets"         ;; the default collection
         ))
